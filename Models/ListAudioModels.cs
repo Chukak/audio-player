061d52aa90ffb02ebb8b928075aac8ef35e4ddfb
Chukak <chukak12@gmail.com>
2019-04-07 23:24:46 +0300

    added new methods and a new property

--- Models/ListAudioModels.cs
+++ Models/ListAudioModels.cs
@@ -8,18 +8,22 @@ namespace audio_player.Models
         private static List<AudioModel> audios = new List<AudioModel>();
         private bool isValid;
 
+        static public int Count = audios.Count;
+
         static public bool AddAudioModel(AudioModel model)
         {
-            if (model.isValid()) {
-                audios.Add(model);
-                return true;
-            }
-            return false;
+            return AddModel(ref model);
         }
 
         public ListAudioModels()
         {
-           isValid = audios.Count > 0;
+            isValid = Count > 0;
+        }
+
+        public bool AddAudioModel(string path)
+        {
+            var model = new AudioModel(path);
+            return AddModel(ref model);
         }
 
         public AudioModel At(int index)
@@ -31,6 +35,16 @@ namespace audio_player.Models
         public List<AudioModel> AudioModels()
         {
             return audios;
+        }
+
+        static private bool AddModel(ref AudioModel model)
+        {
+            if (model.isValid()) {
+                audios.Add(model);
+                ++Count;
+                return true;
+            }
+            return false;
         }
 
         private void checkIndex(int index)
